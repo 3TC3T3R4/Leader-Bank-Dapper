1e90b903e75e42c605762947a95acd66a0bde5f6
Kevin Baquero <baquerochavarro@gmail.com>
2023-03-23 17:05:36 -0500

    Endpoints Card
    Endpoints GET and POST

--- src/leader.bank.domain/Commands/InsertNewCard.cs
+++ src/leader.bank.domain/Commands/InsertNewCard.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace leader.bank.domain.Commands
+﻿namespace leader.bank.domain.Commands
 {
     public class InsertNewCard
     {
         public int Id_Account { get; set; }
         public string NumberCard { get; set; }
         public string Cvc { get; set; }
-        public DateTime EmissionDate { get; set; }
-        public DateTime ExpirationDate { get; set; }
         public string CardState { get; set; }
     }
 }
--- src/leader.bank.domain/Entities/Card.cs
+++ src/leader.bank.domain/Entities/Card.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace leader.bank.domain.Entities
+﻿namespace leader.bank.domain.Entities
 {
     public class Card
     {
@@ -12,8 +6,33 @@ namespace leader.bank.domain.Entities
         public int Id_Account { get; set; }
         public string NumberCard { get; set; }
         public string Cvc { get; set; }
-        public DateTime EmissionDate { get; set; }       
-        public DateTime ExpirationDate { get; set; }
+        public DateTime EmissionDate { get; set; }
+        public DateTime? ExpirationDate { get; set; }
         public string CardState { get; set; }
+
+        public Card() { }
+        public static void Validate(Card card)
+        {
+            if (card.Id_Account == 0 || card.Id_Account == null)
+            {
+                throw new ArgumentException("The account doesn't exist.");
+            }
+            if (card.NumberCard == null)
+            {
+                throw new ArgumentNullException("The card number is required.");
+            }
+            if (card.Cvc == null)
+            {
+                throw new ArgumentNullException("The CVC is required.");
+            }
+            if (card.EmissionDate == null)
+            {
+                throw new ArgumentNullException("The emission date is required.");
+            }
+            if (card.CardState == null)
+            {
+                throw new ArgumentNullException("The card state is required.");
+            }
+        }
     }
 }
